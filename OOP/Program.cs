0959793b66d32446cb3c2a0beeaac439b8dd58c0
Admin <Admin@DESKTOP-ERN2T0A>
2023-10-07 21:22:47 +0700

    Add project files.

---
+++ OOP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var messenge = "hello";
+
+        var studentA = new Student();
+
+        //studentA.Age = 1;
+        studentA.ShowInfo();
+
+        
+    }
+
+    public static void Print(string messenge)
+    {
+        Console.WriteLine(messenge);
+    }
+
+    public class Student
+    {
+        public static string className = "Student";
+
+        private int age;
+
+        public int Age
+        {
+            get {
+                Console.WriteLine("get");
+                return age; 
+                }
+
+            set {
+                Console.WriteLine("set");
+                age = value; 
+                }
+        }
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Age : {age}");
+            var a = Age;
+        }
+    }
+}
